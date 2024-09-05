d73c01649729aea6e51ac2c135025b431cfc1ff9
Miguel Sanabria <miguelsanabria308@gmail.com>
2024-09-05 10:40:16 -0600

    se crea el register

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,41 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens;
 
+    /**
+     * Los atributos que se pueden asignar masivamente.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * Los atributos que se deben ocultar para las matrices.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Los atributos que se deben convertir a tipos nativos.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+];
     // ...
 
     public function workSessions()
